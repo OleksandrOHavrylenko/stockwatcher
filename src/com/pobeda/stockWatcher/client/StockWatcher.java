d1e54350d58f9816671e92538c1ab3b876ae105b
Aleksandr <gavrylenko.aleksandr@gmail.com>
2017-06-07 23:42:09 +0300

    Implementing widgets and panels

--- src/com/pobeda/stockWatcher/client/StockWatcher.java
+++ src/com/pobeda/stockWatcher/client/StockWatcher.java
@@ -1,56 +1,44 @@
 package com.pobeda.stockWatcher.client;
 
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.*;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>
  */
 public class StockWatcher implements EntryPoint {
+    private VerticalPanel mainPanel = new VerticalPanel();
+    private FlexTable stocksFlexTable = new FlexTable();
+    private HorizontalPanel addPanel = new HorizontalPanel();
+    private TextBox newSymbolTextBox = new TextBox();
+    private Button addStockButton = new Button("Add");
+    private Label lastUpdatedLabel = new Label();
 
     /**
      * This is the entry point method.
      */
     public void onModuleLoad() {
-        final Button button = new Button("Click me");
-        final Label label = new Label();
+        // Create table for stock data.
+        stocksFlexTable.setText(0, 0, "Symbol");
+        stocksFlexTable.setText(0, 1, "Price");
+        stocksFlexTable.setText(0, 2, "Change");
+        stocksFlexTable.setText(0, 3, "Remove");
 
-        button.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                if (label.getText().equals("")) {
-                    StockWatcherService.App.getInstance().getMessage("Hello, World!", new MyAsyncCallback(label));
-                } else {
-                    label.setText("");
-                }
-            }
-        });
+        // Assemble Add Stock panel.
+        addPanel.add(newSymbolTextBox);
+        addPanel.add(addStockButton);
 
-        // Assume that the host HTML has elements defined whose
-        // IDs are "slot1", "slot2".  In a real app, you probably would not want
-        // to hard-code IDs.  Instead, you could, for example, search for all
-        // elements with a particular CSS class and replace them with widgets.
-        //
-        RootPanel.get("slot1").add(button);
-        RootPanel.get("slot2").add(label);
-    }
+        // Assemble Main panel.
+        mainPanel.add(stocksFlexTable);
+        mainPanel.add(addPanel);
+        mainPanel.add(lastUpdatedLabel);
 
-    private static class MyAsyncCallback implements AsyncCallback<String> {
-        private Label label;
+        // Associate the Main panel with the HTML host page.
+        RootPanel.get("stockList").add(mainPanel);
 
-        public MyAsyncCallback(Label label) {
-            this.label = label;
-        }
+        // Move cursor focus to the input box.
+        newSymbolTextBox.setFocus(true);
 
-        public void onSuccess(String result) {
-            label.getElement().setInnerHTML(result);
-        }
-
-        public void onFailure(Throwable throwable) {
-            label.setText("Failed to receive answer from server!");
-        }
     }
 }
